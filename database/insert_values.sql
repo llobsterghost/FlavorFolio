63a1a2ccd8af003659a98f1964614423fc1ad87f
clara <clara.tschamon@students.fhv.at>
2023-09-25 19:45:32 +0300

    inserted a few recipes

--- database/insert_values.sql
+++ database/insert_values.sql
@@ -16,5 +16,129 @@ VALUES (1, 'Penne al´ arrabiata', 'A spicy pasta al''arrabiata - or spicy tomat
         5. Drain the pasta and the tomato sauce to it. Serve with shavings of parmesan.\n'
 );
 
+INSERT INTO RECIPE(ID, TITLE, DESCRIPTION, IMAGE, COOKED_COUNTER, STARS, PREPTIME, DIFFICULTY_LEVEL, TYPE, INGREDIENTS, PREPARATION)
+VALUES(2, 'Chocolate Pancakes', 'These fluffy chocolate pancakes are the perfect way for a chocolate lover to start their day! Thick and full of melty chocolate in each bite, this simple recipe gives you a good reason to get out of bed in the morning.',
+        './assets/images/chocolate_pancakes.png', 0, 5, 40, 'Easy', 'Breakfast',
+        ' 1 1/3 cups (185g) all-purpose flour
+        1/4 cup (25g) cocoa powder (I use Dutch-processed)
+        2 teaspoons baking powder
+        1/4 teaspoon salt
+        2 eggs
+        1/3 cup (65g) granulated sugar
+        1 cup (240 ml) whole milk
+        3 tablespoons canola or vegetable oil (or 45g melted butter)
+        1 teaspoon pure vanilla extract
+        1/2 cup (85g) chocolate chips or chunks
+        butter or oil , for cooking
+        140 g (5 oz.) bittersweet or semisweet chocolate
+        1/2 cup (120 ml) heavy cream',
+       'Once you’ve gathered your ingredients, many of which are pantry staples, this recipe comes together in minutes. You sift together your dry ingredients with a fine mesh strainer and, in a separate mixing bowl, whisk together your wet ones. I like to use oil instead of melted butter in this recipe since it makes for a taller, fluffier pancake but feel free to swap for butter if that’s your preference. Or, make both and do a taste test to see which version is your favorite.'
+       'The last step is to pour the wet ingredients into the dry, stirring just until combined. Be careful not to overmix your batter as it will cause your cooked chocolate pancakes to be flat and dense instead of the thick and fluffy ones you’ve been dreaming about the night before. If you’re using chocolate chips, this is the point at which you mix them in. I prefer using a rubber spatula to gently fold them in to avoid overmixing the batter. Once everything is mixed, set the batter aside to rest while you make the chocolate sauce.'
+);
+
+
+INSERT INTO RECIPE(ID, TITLE, DESCRIPTION, IMAGE, COOKED_COUNTER, STARS, PREPTIME, DIFFICULTY_LEVEL, TYPE, INGREDIENTS, PREPARATION)
+VALUES (3, 'Bruschetta', 'Bruschetta is a classic Italian appetizer that is easy to make at home. Toasted bread is topped with tomatoes, Parmesan cheese, garlic, and fresh basil. Use a high-quality balsamic vinegar for best results.',
+        './assets/images/bruschetta.png', 0, 5, 20, 'Easy', 'Appetizer',
+        '1 loaf French bread, cut into 1/4-inch slices' ||
+        '1 tablespoon extra-virgin olive oil' ||
+        '8 roma (plum) tomatoes, diced' ||
+        '⅓ cup chopped fresh basil' ||
+        '1 ounce Parmesan cheese, freshly grated' ||
+        '2 cloves garlic, minced' ||
+        '1 tablespoon good quality balsamic vinegar' ||
+        '2 teaspoons extra-virgin olive oil' ||
+        '¼ teaspoon kosher salt' ||
+        '¼ teaspoon freshly ground black pepper',
+        'Step 1: gather all ingredients\n' ||
+        'Step 2: Preheat oven to 400 degrees F (200 degrees C).\n' ||
+        'Step 3: Brush bread slices on both sides lightly with 1 tablespoon oil and place on large baking sheet. Toast bread until golden, 5 to 10 minutes, turning halfway through. \n' ||
+        'Step 4: Meanwhile, toss together tomatoes, basil, Parmesan cheese, and garlic in a bowl.\n' ||
+        'Step 5: Mix in balsamic vinegar, 2 teaspoons olive oil, kosher salt, and pepper.\n' ||
+        'Step 6: Spoon tomato mixture onto toasted bread slices.\n' ||
+        'Step 7: Serve immediately and enjoy!'
+);
+
+INSERT INTO RECIPE(ID, TITLE, DESCRIPTION, IMAGE, COOKED_COUNTER, STARS, PREPTIME, DIFFICULTY_LEVEL, TYPE, INGREDIENTS, PREPARATION)
+VALUES (4, 'Vegetable Lasagne', 'This vegetarian lasagna is cheesy (of course), and loaded with vegetables, spinach and a simple tomato sauce. If you have any meatless-meal skeptics in your life, this lasagna will change their mind. I’m sure of it.',
+        './assets/images/vegetable_lasagne.png', 0, 5, 60, 'Medium', 'Main dish',
+        'Veggies and spinach \n' ||
+        '- 2 tablespoons extra-virgin olive oil \n' ||
+        '- 3 large carrots, chopped (about 1 cup) \n' ||
+        '- 1 red bell pepper, chopped \n' ||
+        '- 1 medium zucchini, chopped \n' ||
+        '- 1 medium yellow onion, chopped \n' ||
+        '- ¼ teaspoon salt \n' ||
+        '- 5 to 6 ounces baby spinach \n' ||
+        'Tomato sauce (or substitute 2 cups prepared marinara sauce) \n' ||
+        '- 1 large can (28 ounces) diced tomatoes \n' ||
+        '- ¼ cup roughly chopped fresh basil + additional for garnish \n' ||
+        '- 2 tablespoons extra-virgin olive oil \n' ||
+        '- 2 cloves garlic, pressed or minced \n' ||
+        '- ½ teaspoon salt \n' ||
+        '- ¼ teaspoon red pepper flakes \n' ||
+        'Remaining ingredients \n' ||
+        '- 2 cups (16 ounces) low-fat cottage cheese, divided \n' ||
+        '- ¼ teaspoon salt, to taste \n' ||
+        '- Freshly ground black pepper, to taste \n' ||
+        '- 9 no-boil lasagna noodles* \n' ||
+        '- 8 ounces (2 cups) freshly grated low-moisture, part-skim mozzarella cheese',
+        'Preparation Text is really long... here is the link: https://cookieandkate.com/best-vegetable-lasagna-recipe/'
+);
+
+INSERT INTO RECIPE(ID, TITLE, DESCRIPTION, IMAGE, COOKED_COUNTER, STARS, PREPTIME, DIFFICULTY_LEVEL, TYPE, INGREDIENTS, PREPARATION)
+VALUES(5, 'Sachertorte', 'When 16-year-old apprentice chef Franz Sacher created the Sachertorte cake at the court of Prince Metternich in 1832, little did he know the impact his cake would have on chocolate lovers worldwide. The recipe for the Original Sacher-Torte is a well-kept secret, known only to confectioners at Hotel Sacher in Vienna.',
+       './assets/images/sachertorte.png', 0, 5, 120, 'Hard', 'Dessert',
+       'For the cake: \n' ||
+           '- 7 egg yolks \n' ||
+           '- 150 g / 2/3 cup softened butter \n' ||
+           '- 125 g / 1 cup icing sugar \n' ||
+           '- 200 g / 7 oz dark chocolate \n' ||
+           '- 1 packet (8 g) vanilla sugar \n' ||
+           '- 7 egg whites \n' ||
+           '- 125 g / 1/2 cup crystal sugar \n' ||
+           '- A pinch of salt \n' ||
+           '- 150 g / 1 cup flour \n' ||
+           '- Butter and flour for the mould \n' ||
+           '- 150 – 200 g / 5 - 7 oz apricot jam, for spreading \n' ||
+           '- Rum (optional) \n' ||
+       'For the glaze: \n' ||
+            '- 200 g / 7 oz dark chocolate coating or cooking chocolate \n' ||
+            '- 250 g / 1 cup sugar \n' ||
+            '- 150 - 170 ml / 2/3 cup water',
+       'Step 1: \n' ||
+       ' Melt the chocolate slowly (ideally in a bain-marie). Meanwhile, mix the butter with the icing sugar and vanilla sugar until creamed. Gradually stir in the egg yolks. Pre-heat the oven to 180°C / 356°F.\n\n'
+       'Step 2: \n' ||
+       'Grease a cake tin with butter and sprinkle with flour. Whip up the egg whites with a pinch of salt, add the crystal sugar and beat to a stiff peak. Stir the melted chocolate into the paste with the egg yolks and fold in the whipped egg whites alternately with the flour. Fill the dough into the tin and bake for around 1 hour. \n\n' ||
+       'Step 3: \n' ||
+       'Remove the cake and leave to cool off (to achieve a flat surface turn the cake out on to a work surface immediately after baking and turn it again after 25 minutes).
+       If the apricot jam is too solid, heat it briefly, and stir until smooth, before flavouring with a shot of rum. Cut the cake in half crosswise. Cover the base with jam, set the other half on top, and coat the upper surface and around the edges with apricot jam. \n\n' ||
+       'Step 4:\n' ||
+       'For the glaze, break the chocolate into small pieces. Heat up the water with the sugar for a few minutes. Pour into a bowl and leave to cool down until just warm to the taste (if the glaze is too hot it will become dull in appearance, but if too cold it will become too viscous). Add the chocolate and dissolve in the sugar solution. Pour the glaze quickly, i.e. in a single action, over the cake and immediately spread it out and smooth it over the surface, using a palate knife or other broad-bladed knife. Leave the cake to dry at room temperature. \n\n
+        Serve with a garnish of whipped cream. If possible, do not store the Sachertorte in the fridge, as it will “sweat”.'
+);
+
+INSERT INTO RECIPE(ID, TITLE, DESCRIPTION, IMAGE, COOKED_COUNTER, STARS, PREPTIME, DIFFICULTY_LEVEL, TYPE, INGREDIENTS, PREPARATION)
+VALUES(6, 'Greek Salad', 'This easy Greek salad recipe is our new favorite summer side dish! Just 7 ingredients + a simple Greek salad dressing make it refreshing & delicious.',
+        './assets/images/greek_salad.png', 0, 5, 20, 'Easy', 'Appetizer',
+        'Dressing: \n' ||
+        '- ¼ cup extra-virgin olive oil \n' ||
+        '- 3 tablespoons red wine vinegar \n' ||
+        '- 1 garlic clove, minced \n' ||
+        '- ½ teaspoon dried oregano, more for sprinkling \n' ||
+        '- ¼ teaspoon Dijon mustard \n' ||
+        '- ¼ teaspoon sea salt \n' ||
+        '- Freshly ground black pepper \n' ||
+        'Salad: \n' ||
+        '- 1 English cucumber, cut lengthwise, seeded, and sliced ¼-inch thick \n' ||
+        '- 1 green bell pepper, chopped into 1-inch pieces \n' ||
+        '- 2 cups halved cherry tomatoes \n' ||
+        '- 5 ounces feta cheese, cut into ½ inch cubes* \n' ||
+        '- ⅓ cup thinly sliced red onion \n' ||
+        '- ⅓ cup pitted Kalamata olives \n' ||
+        '- ⅓ cup fresh mint leaves',
+        '1. Make the dressing: In a small bowl, whisk together the olive oil, vinegar, garlic, oregano, mustard, salt, and several grinds of pepper. \n' ||
+        '2. On a large platter, arrange the cucumber, green pepper, cherry tomatoes, feta cheese, red onions, and olives. Drizzle with the dressing and very gently toss. Sprinkle with a few generous pinches of oregano and top with the mint leaves. Season to taste and serve.'
+);
 
 
