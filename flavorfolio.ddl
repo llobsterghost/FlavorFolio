fd9fdea655bc137a060040bfa7ad03589e6f1523
clara <clara.tschamon@students.fhv.at>
2023-09-25 15:59:09 +0300

    ddl and first recipe

---
+++ flavorfolio.ddl
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS favourites;
+
+CREATE TABLE recipe (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        image VARCHAR(255),
+        cooked_counter INT DEFAULT 0,
+        stars DECIMAL(2, 1) DEFAULT 0.0 CHECK (stars >= 0 AND stars <= 5),
+        preptime INT NOT NULL,
+        difficulty_level VARCHAR(255) NOT NULL,
+        type VARCHAR(255),
+        ingredients TEXT NOT NULL,
+        preparation TEXT NOT NULL
+);
+
+CREATE TABLE favourites (
+        recipe_id INT PRIMARY KEY,
+        FOREIGN KEY (recipe_id) REFERENCES recipe(id)
+);
---
+++ insert_values.sql
@@ -0,0 +1,16 @@
+INSERT INTO RECIPE(ID, TITLE, DESCRIPTION, IMAGE, COOKED_COUNTER, STARS, PREPTIME, DIFFICULTY_LEVEL, TYPE, INGREDIENTS, PREPARATION)
+VALUES (1, 'Penne al´ arrabiata', 'A spicy pasta al''arrabiata - or spicy tomato sauce to you and me - an easy storecupboard pasta dish for a weeknight supper.',
+        './assets/images/penne_arrabiata.png', 0, 5, 30, 'Easy', 'Main dish',
+        '6 tbsp extra virgin olive oil
+        2 red chillies, finely sliced
+        2 garlic cloves, chopped
+        handful basil leaves
+        600g/1lb 5oz tinned chopped tomatoes
+        salt, to taste
+        400g/14oz penne pasta
+        parmesan shavings (or similar vegetarian hard cheese), to serve',
+        '1. Heat the olive oil in a frying pan. Add the chilli and garlic to the pan. After about a minute, add the basil leaves and gently wilt them in the flavoured oil.\n
+        2. Remove the garlic, basil and chilli from the pan and set aside. Add the chopped tomatoes to the frying pan.\n
+        3. Put the garlic, basil and chilli back in the pan with the tomatoes. Simmer for at least 10 minutes, until the sauce has thickened a little. Add salt to taste.\n
+        4. Meanwhile, cook the pasta according to the packet instructions in large saucepan of boiling, salted water until al dente.\n
+        5. Drain the pasta and the tomato sauce to it. Serve with shavings of parmesan.\n')
